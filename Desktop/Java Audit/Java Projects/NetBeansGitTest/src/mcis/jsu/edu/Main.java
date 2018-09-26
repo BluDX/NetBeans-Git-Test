32c38b161a0654fb219a72d18613bac6fa32fb7b
Blu <36525439+CassidyBlu82@users.noreply.github.com>
2018-09-25 21:42:00 -0500

    Initial Commit

---
+++ Desktop/Java Audit/Java Projects/NetBeansGitTest/src/mcis/jsu/edu/Main.java
@@ -0,0 +1,10 @@
+package mcis.jsu.edu;
+
+
+import mcis.jsu.edu.TestClass;
+
+public class Main {
+   public static void main(String[] args) {
+System.out.println(TestClass.testMethod1());
+} 
+}
---
+++ Desktop/Java Audit/Java Projects/NetBeansGitTest/src/mcis/jsu/edu/TestClass.java
@@ -0,0 +1,7 @@
+package mcis.jsu.edu;
+
+public class TestClass {
+    public static String testMethod1() {
+return "Hello, World!";
+}
+}
